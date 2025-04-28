66d25bdb0b93cf50ebf678df0db735b28b195f7d
SangpilAn <asp4303@gmail.com>
2025-04-29 00:52:48 +0900

    asp-board First Commit

---
+++ service/article/src/test/java/asp/board/article/api/ArticleApiTest.java
@@ -0,0 +1,79 @@
+package asp.board.article.api;
+
+import asp.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest(){
+        ArticleCreateRequest request = new ArticleCreateRequest(
+                "hi", "my Content", 1L, 1L
+        );
+        ArticleResponse response = create(request);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request){
+        return restClient.post()
+                .uri("/v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void readTest(){
+        ArticleResponse response = read(175271436124024832L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId){
+        return restClient.get()
+                .uri("/v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest(){
+        ArticleResponse response = update(175267270826893312L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse update(Long articleId){
+        return restClient.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi 2", "my Content 22"))
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void deleteTest(){
+        restClient.delete()
+                .uri("/v1/articles/175271436124024832")
+                .retrieve();
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+        private String title;
+        private String content;
+        private Long boardId;
+        private Long writerId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
